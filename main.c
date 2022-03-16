21e35049f782da3898560c03698b93a85aa01a0d
amirg00 <amirg00@walla.com>
2022-03-16 13:55:37 +0200

    ADD 'TCP PORT' COMMAND AS WELL AS SERVER AND CLIENT TCP SOCKET CONNECTION

--- main.c
+++ main.c
@@ -2,26 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <winsock2.h>
+#include <sys/socket.h>
 #include <dirent.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define Localhost "127.0.0.1"
+#define SERVER_PORT 5060
 
-
-void getDirFiles(){
+void getDirFiles(int tcp_port_flag, int sock){
     char curr_working_dir[256];
     if(getcwd(curr_working_dir, sizeof(curr_working_dir)) != NULL){
         DIR *dir = opendir(curr_working_dir);
         struct dirent *entry;
-        while((entry = readdir(dir))){
-            printf("%s\n", entry->d_name);  //prints a dir file's name.
+        while((entry = readdir(dir))) {
+            if (tcp_port_flag) {
+                send(sock, entry->d_name, strlen(entry->d_name) ,0);
+            } else {
+                printf("%s\n", entry->d_name);  //prints a dir file's name.
+            }
         }
         closedir(dir);
     }
     else{
         perror("Unable to read the directory!");
     }
-
 }
 
 char* getInput(){
@@ -51,6 +56,11 @@ int main() {
     /****************** Start Of The Shell *******************/
     //--------------------------------------------------------/
 
+    int sock; // tcp socket for TCP PORT command.
+    struct sockaddr_in server_addr;
+    socklen_t dataLenSock;
+    int TCP_PORT_FLAG = 0; // FLAG IS OFF
+
     printf("-------------------------------------\n"
            "********** Welcome To Shell! ********\n"
            "-------------------------------------\n");
@@ -61,12 +71,36 @@ int main() {
         }
         char* user_in = getInput();
 
+        //--------------------------------------------------------/
+        /******************  Commands Section  *******************/
+        //--------------------------------------------------------/
 
         if (!strncmp("ECHO ", user_in, strlen("ECHO "))){
             user_in += sizeof (char) * strlen("ECHO ");
             printf("%s\n", user_in);
         }
 
+        else if(!strcmp(user_in, "TCP PORT")){
+            TCP_PORT_FLAG = 1; // SET FLAG TO TRUE
+            sock = socket(AF_INET, SOCK_STREAM, 0);
+            if (sock < 0){
+                perror("Error in socket");
+                continue;
+            }
+            bzero(&server_addr, sizeof(server_addr));
+            server_addr.sin_family      = AF_INET;
+            server_addr.sin_port        = htons(SERVER_PORT);
+            server_addr.sin_addr.s_addr = inet_addr(Localhost);
+
+            int connRes = connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
+            if (connRes == -1){
+                perror("Error in socket");
+                continue;
+            }
+            printf("connect succeeded\n");
+            write(sock, "hey", strlen("hey"));
+
+        }
 
         else if (!strcmp(user_in, "LOCAL")){
             printf("TCP Connection...\n");
@@ -74,7 +108,7 @@ int main() {
 
         // Syntax: COPY <SRC> <DEST>
         else if (!strcmp(user_in, "DIR")){
-            getDirFiles();
+            getDirFiles(TCP_PORT_FLAG, sock);
         }
 
         else if (!strncmp("CD ", user_in, strlen("CD "))){
@@ -84,13 +118,12 @@ int main() {
             }
         }
 
-        else if (!strcmp(user_in, "TCP PORT")){
-            printf("TCP Connection...\n");
-        }
-
         else if (!strcmp(user_in, "EXIT")){
             printf("Exiting...\n");
             free(user_in);
+            if (sock >= 0){
+                close(sock);
+            }
             exit(1);
         }
         else{
---
+++ server.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <dirent.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define Localhost "127.0.0.1"
+#define PORT 5060
+#define SIZE 1024
+
+int main() {
+
+    int sock, clisock, bind_response;
+    struct sockaddr_in server_addr, new_addr;
+    socklen_t addr_size;
+    char buff[SIZE] = {0};
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Error in socket\n");
+        exit(1);
+    }
+    printf("Server socket created successfully.\n");
+
+    // set the server
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family      = AF_INET;
+    server_addr.sin_port        = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    // bind server
+    printf("bind\n");
+    bind_response = bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (bind_response < 0) {
+        perror("Error: couldn't bind()\n");
+        exit(1);
+    }
+    printf("Bind() successful\n");
+
+    if (listen(sock, 1) == 0) {
+        printf("Listening for any income connections...\n");
+    } else {
+        perror("Listen failed! something has occurred while listening.\n");
+        exit(1);
+    }
+    printf("User connected");
+    addr_size = sizeof(clisock);
+    clisock = accept(sock, (struct sockaddr *) &new_addr, &addr_size);
+    if (clisock < 0){
+        perror("server accept has failed!");
+        exit(1);
+    }
+
+    while (1) {
+        bzero(buff, SIZE);
+        size_t bytes = read(clisock, buff, SIZE);
+        printf("%zu", bytes);
+        if (bytes < 0) {
+            perror("recv failed");
+        } else if (bytes == 0) {
+            break;
+        } else {
+            printf("%s", buff);
+            bzero(buff, SIZE);
+        }
+    }
+    // closing resources:
+    close(clisock);
+    close(sock);
+    return 0;
+}
+
